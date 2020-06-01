b796a078473bdd597b01161d73a1a9a4de3968ec
outflows <8183636+outflows@users.noreply.github.com>
2020-06-01 18:55:24 -0300

    initial commit

---
+++ churchill.py
@@ -0,0 +1,23 @@
+import random
+import matplotlib.pyplot as plt
+
+with open("frases.txt") as f:
+    frases = f.readlines()
+frases = [x.strip() for x in frases]
+
+frase = random.choice(frases)
+churchill = "Winston Churchill"
+
+xmin, xmax = 20, 1180
+ymin, ymax = 250, 975
+im = plt.imread("churchill.jpeg")
+plt.imshow(im)
+props = dict(boxstyle='square', pad=0.3, facecolor='#2B292A', alpha=0)
+plt.text(xmin, ymin, frase, fontsize=16, verticalalignment='top', bbox=props, color='white')
+plt.text(285, 1050, churchill, fontsize=14, verticalalignment='top', color='white', style='italic')
+plt.xticks([])
+plt.yticks([])
+plt.tight_layout()
+
+#plt.show()
+plt.savefig("frase.png", dpi = 150)
